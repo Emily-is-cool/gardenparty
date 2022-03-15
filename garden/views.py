c89a5115ffe3a71aa2d5423b537050fa0e258be0
Emily-is-cool <79616525+Emily-is-cool@users.noreply.github.com>
2022-03-15 13:19:26 -0700

    first views

--- garden/views.py
+++ garden/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from importlib.resources import Resource
+from django.shortcuts import get_object_or_404, render
+from .models import Garden, GardenType, Plant
+from django.urls import reverse_lazy
+##from .forms import ADD FORM NAMES HERE
+from django.contrib.auth.decorators import login_required
+
+
 
 # Create your views here.
 def index(request):
     return render(request, 'garden/index.html')
+
+def gardens(request):
+    garden_list=Garden.objects.all()
+    return render(request, 'garden/gardens.html', {'garden_list': garden_list})
